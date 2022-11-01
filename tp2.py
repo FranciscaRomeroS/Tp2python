e6bd95389964b6c6635f09b43c36cc0f7fda8fd6
FranciscaRomeroS <101876889+FranciscaRomeroS@users.noreply.github.com>
2022-11-01 11:03:08 -0300

    Add files via upload

---
+++ tp2.py
@@ -0,0 +1,85 @@
+""" >>>> NO TOCAR ESTE CÓDIGO >>>> """
+
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+def str2datetime(date, fmt="%Y-%m-%d"):
+    """Convierte una cadena (o secuencia de cadenas) a tipo datetime (o secuencia de datetimes).
+
+    ENTRADAS:
+        date (str ó secuencia de str): fechas a convertir.
+        fmt (str, opcional): formato de fecha (ver documentación de biblioteca datetime).
+    SALIDAS:
+        output (datetime ó secuencia de datetime): fechas convertidas a datetime.
+    EJEMPLOS:
+    >>>> date = str2datetime('2022-10-24')
+    >>>> print(date.year, date.month, date.day)
+
+    >>>> date = str2datetime(['2022-10-24', '2022-10-23', '2022-10-22'])
+    >>>> print(len(date))"""
+    if isinstance(date, str):
+        return datetime.strptime(date, fmt)
+    elif isinstance(date, (list, np.ndarray)):
+        output = []
+        for d in date:
+            output.append(datetime.strptime(d, fmt))
+        if isinstance(date, np.ndarray):
+            output = np.array(output)
+        return output
+
+
+def datetime2str(date, fmt="%Y-%m-%d"):
+    """Convierte un datetime (o secuencia de datetimes) a tipo str (o secuencia de str).
+
+    ENTRADAS:
+        date (datetime ó secuencia de datetime): fechas a convertir.
+        fmt (str, opcional): formato de fecha (ver documentación de biblioteca datetime).
+    SALIDAS:
+        output (str ó secuencia de str): fechas convertidas a cadenas.
+    EJEMPLOS:
+    >>>> date_str = '2022-10-24'
+    >>>> date = str2datetime(date_str)
+    >>>> print(datetime2str(date) == date_str)"""
+    if isinstance(date, datetime):
+        return date.strftime(fmt)
+    elif isinstance(date, (list, np.ndarray)):
+        output = []
+        for d in date:
+            output.append(d.strftime(fmt))
+        if isinstance(date, np.ndarray):
+            output = np.array(output)
+        return output
+
+
+""" >>>> DEFINAN SUS FUNCIONES A PARTIR DE AQUÍ >>>> """
+
+def readfile (archivo):
+    dict= { }
+    with open (archivo, 'r') as file:
+        for i, linea in enumerate(file, 1):
+            linea= linea.rstrip('\n')
+            if i == 1:
+                nombre_columnas= linea.split(',')
+                for nombre in nombre_columnas:
+                    dict[nombre.strip()]= []
+            else:
+                datos_linea= linea.split(',')
+                for i, datos in enumerate (datos_linea):
+                    if i == 0:
+                        date= str2datetime(datos)
+                        dict[nombre_columnas[i].strip()].append(date)
+                    else:
+                        dict[nombre_columnas[i].strip()].append(float(datos))
+                    
+    return dict
+
+""" def monthly_average(accion, diccionario): """
+
+
+""" >>>> ESCRIBAN SU CÓDIGO A PARTIR DE AQUÍ >>>> """
+
+
+
+
